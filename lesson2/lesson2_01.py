c015899e8899e81735c8f1d6794c81e6def18624
zshy <2482085135@qq.com>
2018-06-20 09:48:41 +0800

    add readme.md

---
+++ lesson2/lesson2_01.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+x = tf.constant([23,34,56], name='x')
+y = tf.Variable(x + 5, name = 'y')
+
+model = tf.global_variables_initializer()
+
+with tf.Session() as session:
+	session.run(model)
+	print(session.run(y))
